f7b4dadfa567a4ea054bc707a05ca6901506943d
Masaki Koakutsu <masakiika3399@gmail.com>
2025-01-14 21:59:30 +0900

    no message

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -32,7 +32,7 @@
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
 
                                 @error('email')
-                                    <span class="invalid-feedback" role="alert">
+                                    <span class="invalid-feedback" role="alert"
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
