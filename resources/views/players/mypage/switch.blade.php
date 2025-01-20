2291058e4476733a31c95aa222ab29c60e89af7e
Shizuka Haraikawa <shizuka.haraikawa@gmail.com>
2025-01-20 20:41:06 +0900

    Add Icon + Account Menu to switch page

--- resources/views/players/mypage/switch.blade.php
+++ resources/views/players/mypage/switch.blade.php
@@ -1,12 +1,89 @@
 @extends('layouts.app')
 @section('title', 'Switch to Quest Creator')
 @section('content')
+
+<style>
+
+ /* 1　左のバー */
+ .side-bar{
+            border: 2px solid #261C11;
+            border-radius: 20px;
+            font-size: 6px;
+            margin-left: 30px;
+        }
+/* 2　剣持った画像の事 */
+    .player-image{
+        height: 100px;
+        width: 100px;
+        margin-top: 30px;
+        margin-bottom: 15px;
+    }
+/* 3　ファイルを選択の下の2行の事 */
+    .Accetable{
+        font-size: 10px;
+        line-height: 1.2; /* 行間を狭める */
+        margin-bottom: 5px; /* パラグラフ間の余白も調整 */
+        margin-top: 10px;
+    }
+/* 4　4つある剣の画像の事 */
+    .sword{
+        height: 40px;
+        width: 40px;
+    }
+</style>
+   
 <div class="row text-center">
-    {{-- Icon + Account Menu --}}
-    <div class="col-3 bg-warning">
-        <h2>   Icon + Account Menu   </h2>
+
+
+
+
+
+
+{{-- 1　Icon + Account Menu --}}
+<div class="row text-center">
+    <div class="side-bar col-2 bg-white">
+{{-- 2 --}}
+        <div>
+            <img src="{{ asset('images/User icon.png')}}" alt="playerimage" class="player-image">
+        </div>
+        <div class="mb-3">
+            <input type="file" name="creator_image" id="image" class="form-control form-control-sm mt-1" aria-describedby="avatar-info">
+                    @error('avatar')
+                        <p class="text-danger small">{{ $message }}</p>
+                    @enderror
+{{-- 3 --}}
+            <div class="Accetable" style="text-align: center">
+                <p>Accetable formats:jpeg,jpg,png,gif only.</p>
+                <p>Max file size: 1048kB</p>
+            </div>
+        </div>
+{{-- 4 --}}
+        <div style="text-align: left">
+            <img src={{ asset('images/sword.png') }} alt="sword" class="sword">
+            <a href="#" class="text-decoration-none fs-6 text-dark">Account Securlty</a>
+        </div>
+        <div style="text-align: left">
+            <img src={{ asset('images/sword.png') }} alt="sword" class="sword">
+            <a href="#" class="text-decoration-none fs-6 text-dark">About Us</a>
+        </div>
+        <div style="text-align: left">
+            <img src={{ asset('images/sword.png') }} alt="sword" class="sword">
+            <a href="#" class="text-decoration-none fs-6 text-dark">Switch to Quest Creator Account</a>
+        </div>
+        <div style="text-align: left">
+            <img src={{ asset('images/sword.png') }} alt="sword" class="sword">
+            <a href="#" class="text-decoration-none fs-6 text-dark">Delete My Account</a>
+        </div>
     </div>
-    {{-- Form --}}
+
+
+
+
+
+
+
+
+{{-- Form --}}
     <div class="col-9">
         <form action="{{ route('questcreator.store') }}" method="post" enctype="multipart/form-data">
         @csrf
