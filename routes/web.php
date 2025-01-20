934cbc809626f2a9699679793925a22756911a69
古川紗綺 <furukawa@n2p.co.jp>
2025-01-20 22:37:21 +0900

    add: model,view,controller for Quest Create Page

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\QuestCreatorController;
+use App\Http\Controllers\QuestController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -12,3 +13,5 @@ Route::get('/home', [HomeController::class, 'index'])->name('home');
 Route::get('/test', [UserController::class, 'viewTestSwitch']);
 Route::post('/questcreator/store',[QuestCreatorController::class,'store'])->name('questcreator.store');
 Route::get('/creatorMyPage', [QuestCreatorController::class, 'viewCreatorMyPage'])->name('questcreators.creatorMyPage');
+
+Route::get('/create',[QuestController::class,'viewCreateQuest'])->name('quests.create');
