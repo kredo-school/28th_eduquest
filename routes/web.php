2b52f649a06a274570458dccc01985a8420b5b24
ItoneYoneda <y.itone7@gmail.com>
2025-01-21 14:19:24 +0900

    Layouts app.blade.php - Edit from '$user->xxxx' to 'Auth::user()->xxxx'

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 Auth::routes();
-Route::get('/', [HomeController::class, 'index'])->name('home');
+// Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/home', [HomeController::class, 'showNews'])->name('news');
 Route::get('/test', [UserController::class, 'viewTestSwitch']);
 Route::post('/questcreator/store',[QuestCreatorController::class,'store'])->name('questcreator.store');
