077ec3089c62ef2cb14c739bc96b125de1c52e10
Nonoka Matsuo <nonomomo0613@gmail.com>
2025-01-21 21:58:00 +0900

    route

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/player/chapterlist', [ChapterlistController::class, 'viewChapterList']);
