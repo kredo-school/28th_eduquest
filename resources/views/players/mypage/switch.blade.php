d33de8d6b5535a5b82b16a08ecc6c1b707d19ed7
ItoneYoneda <y.itone7@gmail.com>
2025-01-31 01:12:46 +0900

    Separate css to style.css

--- resources/views/players/mypage/switch.blade.php
+++ resources/views/players/mypage/switch.blade.php
@@ -1,56 +1,8 @@
 @extends('layouts.app')
+
 @section('title', 'Switch to Quest Creator')
+
 @section('content')
-
-<style>
-
- /* 1　左のバー */
-.side-bar{
-        border: 2px solid #261C11;
-        border-radius: 20px;
-        font-size: 6px;
-        margin-left: 30px;
-        /* margin-bottom: 50px;  */
-        display: flex;
-        flex-direction: column; /* 縦方向に並べる */
-        /* height: 100%;  */
-    }
-/* 2　剣持った画像の事 */
-    .player-image{
-        height: 100px;
-        width: 100px;
-        margin-top: 30px;
-        margin-bottom: 15px;
-    }
-/* 3　ファイルを選択の下の2行の事 */
-    .Accetable{
-        font-size: 10px;
-        line-height: 1.2; /* 行間を狭める */
-        margin-bottom: 5px; /* パラグラフ間の余白も調整 */
-        margin-top: 10px;
-    }
-/* 4　3つある剣の画像の事 */
-    .sword{
-        height: 40px;
-        width: 40px;
-    }
-
-    .slime {
-        height: 40px;
-    }
-
-    .custom-switch-btn {
-    background-color: #588157;
-    color: white;
-    text-decoration: none;
-    display: inline-block;
-    margin-right: 15px;
-    padding-top: 10px;
-    padding-bottom: 10px;
-    text-align: center;
-    width: 130px;
-}
-</style>
    
 <div class="row align-items-start">
     {{-- 1　Icon + Account Menu --}}
@@ -72,19 +24,19 @@
         </div>
         {{-- 4 --}}
         <div style="text-align: left">
-            <img src={{ asset('images/sword.png') }} alt="sword" class="sword">
+            <img src={{ asset('images/sword.png') }} alt="sword" class="homeSword">
             <a href="#" class="text-decoration-none fs-6 text-dark">Account Securlty</a>
         </div>
             @auth
                 @if ( auth()->user()->role_id === 1)
                     <div style="text-align: left">
-                        <img src={{ asset('images/sword.png') }} alt="sword" class="sword">
+                        <img src={{ asset('images/sword.png') }} alt="sword" class="homeSword">
                         <a href="#" class="text-decoration-none fs-6 text-dark">Switch to Quest Creator Account</a>
                     </div>
                 @endif
             @endauth
         <div style="text-align: left">
-            <img src={{ asset('images/sword.png') }} alt="sword" class="sword">
+            <img src={{ asset('images/sword.png') }} alt="sword" class="homeSword">
             <a href="#" class="text-decoration-none fs-6 text-dark">Delete My Account</a>
         </div>
     </div>
