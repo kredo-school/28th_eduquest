aa1fcc3d87cf6ce07a6cf55a23675ddf0fdf9c3d
ItoneYoneda <y.itone7@gmail.com>
2025-02-02 20:23:56 +0900

    inprogress

---
+++ app/Http/Controllers/StatusController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use App\Models\Quest;
+
+class StatusController extends Controller
+{
+    
+    public function viewQuestStatus($id)
+    {
+        return view('players.quests.queststatus');    
+    }
+
+
+
+}
---
+++ resources/views/players/quests/queststatus.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('title', 'Quest Status')
+
+@section('content')
+<div class="row gx-5">
+    <div class="p-4 position-relative" style="background: url('{{ asset('images/Group 209.png') }}') no-repeat center center; background-size:cover; height: 10px; width: auto;">
+
+</div>
+@endsection
