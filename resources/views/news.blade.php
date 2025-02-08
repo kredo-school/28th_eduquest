256d4a7debf524c1b69f9443f756f97bef7d0584
Masaki Koakutsu <masakiika3399@gmail.com>
2025-02-08 15:02:52 +0900

    complete

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -4,18 +4,23 @@
 
 @section('content')
 <div class="container mt-5">
-    <h1>News</h1>
+    <h1 class="text-center mb-5">News</h1>
+    <hr>
     <div class="news-item">
-        <h2>Top 10 Popular Classes of November 2024</h2>
-        <p>December 23, 2024</p>
+        <h2>Top 10 Most Popular Quest of 2024</h2>
+        <p>January 15, 2025</p>
     </div>
+    <hr>
     <div class="news-item">
-        <h2>XXXX</h2>
-        <p>November 25, 2024</p>
+        <h2>Top 5 Recommended Quest for Learning AI</h2>
+        <p>January 8, 2025</p>
     </div>
+    <hr>
     <div class="news-item">
-        <h2>XXXX</h2>
-        <p>November 10, 2024</p>
+        <h2>Regarding Inquiries During the Year-End and New Year Holidays</h2>
+        <p>December 20, 2024</p>
     </div>
+    <hr>
+    <p>and more...</p>
 </div>
 @endsection 
