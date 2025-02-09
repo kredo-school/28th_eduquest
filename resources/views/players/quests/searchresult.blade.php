7f1ebaebaf4bcdb125bed544d98e57307a88da53
ItoneYoneda <y.itone7@gmail.com>
2025-02-09 15:36:03 +0900

    Completed

--- resources/views/players/quests/searchresult.blade.php
+++ resources/views/players/quests/searchresult.blade.php
@@ -42,11 +42,7 @@
 <div class="container">
 
     <h1 class="mb-4">
-        Search Results
-        @php
-            // どのようなフォーマットで表示するか
-            // 例: - "カテゴリー名 / 検索ワード"
-        @endphp
+        <img src="{{ asset('images/flag_green.png') }}" alt="flag_green" class="flag_green"> Search Results
         @if($categoryName && $searchWord)
            - "{{ $categoryName }} / {{ $searchWord }}"
         @elseif($categoryName)
@@ -59,34 +55,40 @@
     @if($quests->isEmpty())
         <p>No quests found.</p>
     @else
-        <!-- 横スクロールで表示 -->
-        <div class="horizontal-scroll">
+        <!-- 行に対して、自動で6列 (row-cols-6) -->
+        <div class="row row-cols-2 row-cols-md-3 row-cols-lg-6 g-5">
             @foreach($quests as $quest)
-                <div class="quest-card p-2">
-                    
-                    <!-- サムネイル -->
-                    <img src="{{ $quest->thumbnail }}" alt="Thumbnail">
-
-                    <!-- タイトル -->
-                    <h5 class="mt-2 mb-1">{{ $quest->quest_title }}</h5>
-
-                    <!-- Creatorアイコン + 名前 -->
-                    @if($quest->creator)
-                        <div class="creator-info">
-                            @if($quest->creator->creator_image)
-                                <img src="{{ $quest->creator->creator_image }}" alt="Creator Icon">
-                            @else
+                <div class="col">
+                    <div class="quest-card p-2">
+                        <!-- Thumbnail -->
+                        <a href="{{ route('quests.chapters', $quest->id) }}">
+                            <img src="{{ $quest->thumbnail }}" alt="Thumbnail">
+                        </a>
+                        <!-- Title -->
+                        <a href="{{ route('quests.chapters', $quest->id) }}" class="text-dark text-decoration-none">
+                            <h5 class="mt-2 mb-1">{{ $quest->quest_title }}</h5>
+                        </a>
+                        <!-- Creator Icon + Name -->
+                        @if($quest->creator)
+                            <div class="creator-info">
+                                <a href="{{ route('questcreators.profile.view', $quest->creator->user_id) }}">
+                                    @if($quest->creator->creator_image)
+                                        <img src="{{ $quest->creator->creator_image }}" alt="Creator Icon">
+                                    @else
+                                        <i class="fas fa-user"></i>
+                                    @endif
+                                </a>
+                                <a href="{{ route('questcreators.profile.view', $quest->creator->user_id) }}" class="text-dark text-decoration-none">
+                                    <span>{{ $quest->creator->creator_name }}</span>
+                                </a>
+                            </div>
+                        @else
+                            <div class="creator-info">
                                 <i class="fas fa-user"></i>
-                            @endif
-                            <span>{{ $quest->creator->creator_name }}</span>
-                        </div>
-                    @else
-                        <div class="creator-info">
-                            <i class="fas fa-user"></i>
-                            <span>Unknown Creator</span>
-                        </div>
-                    @endif
-
+                                <span>Unknown Creator</span>
+                            </div>
+                        @endif
+                    </div>
                 </div>
             @endforeach
         </div>
